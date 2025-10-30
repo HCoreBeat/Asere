323d714eedea3054568e1adf26408214d7857922
HCoreBeat <alederibia@gmail.com>
2025-10-29 22:23:37 -0400

    Delete Js/service-worker.js

--- Js/service-worker.js
+++
@@ -1,7 +0,0 @@
-// service-worker.js
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-    event.waitUntil(
-        clients.openWindow('https://asereshops.com') // Cambia a la URL de tu página web
-    );
-});
